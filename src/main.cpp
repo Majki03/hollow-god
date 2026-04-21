55858f30b8801504760f481b91609f4d1de693c6
Majki03 <majki03@int.pl>
2026-04-21 22:29:12 +0200

    handle window close + escape-to-quit

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,18 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1280, 720), "The Hollow God");
 
     while (window.isOpen()) {
-        window.clear();
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed &&
+                event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(10, 8, 14));
         window.display();
     }
 
